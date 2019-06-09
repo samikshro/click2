0c4db333c2e4597400b3bc4763714c863824fc36
Samik Shrotriya <ss2659@cornell.edu>
2019-06-09 16:24:15 -0400

    Initial Commit

---
+++ Click2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Click2
+//
+//  Created by Samik Shrotriya on 6/9/19.
+//  Copyright © 2019 Click. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
